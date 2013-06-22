c6890275c13658ff59c5daf9df338087912c54ed
emiliotelevision@gmail.com <emiliotelevision@gmail.com>
2013-06-22 08:57:03 -0400

    sales page

--- client/template_js~/ribbon.js
+++ client/template_js~/ribbon.js
@@ -1,18 +1,24 @@
-Template.ribbon.events({
-	'click .announcements': function() {
-		if($('#ribbon').is(':visible')) {
-			$('#ribbon').animate({
-				opacity: 0,
-				top: '-=500'
-			}, 500, 'easeOutExpo', function() {
-				$(this).hide();
-			});
-		}
-		else {
-			$('#ribbon').show().animate({
-				opacity: 1,
-				top: '+=500'
-			}, 500, 'easeOutExpo');
-		}
-	}
+Meteor.startup(function() {
+	$('.announcements').live('mouseenter', function() {
+		$('#ribbon').show().animate({
+			opacity: 1,
+			top: 0
+		}, 200, 'easeOutExpo');
+	});
+	$('#ribbon').live('mouseleave', function() {
+		$('#ribbon').animate({
+			opacity: 0,
+			top: -500
+		}, 400, 'easeOutExpo', function() {
+			$(this).hide();
+		});
+	});
+	
+	$('.tap, .order').live('mouseenter', function() {
+		$('.tap').css('transform', 'rotate(10deg)');
+	});
+	$('.tap, .order').live('mouseleave', function() {
+		$('.tap').css('transform', 'rotate(0deg)');
+	});
+	
 });
